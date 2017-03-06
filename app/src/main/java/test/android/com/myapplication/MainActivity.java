29fe914bba438da466d12a498e5d695d5a6e272f
rasheedmuhammed <muhammed.rasheed@citrusinformatics.com>
2017-03-06 17:18:17 +0530

    hello- new branch

--- app/src/main/java/test/android/com/myapplication/MainActivity.java
+++ app/src/main/java/test/android/com/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package test.android.com.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +10,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        adds(5,6);
 
-// New Branch
+
+
+       // New Branch
+
+    }
+
+    private void adds(int i, int i1) {
+        int sum=i+i1;
+        Toast.makeText(MainActivity.this,"SUM IS:"+sum,Toast.LENGTH_SHORT).show();
+        crateToast("SUM IS:"+sum);
+    }
+
+
+
+    private void crateToast(String s) {
+        Toast.makeText(MainActivity.this,s,Toast.LENGTH_SHORT).show();
+
     }
 }
